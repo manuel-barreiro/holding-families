207b97c4ec301c699bae3bd4527c2d1d4326f94f
Manuel <manuel.barreiro@ing.austral.edu.ar>
2025-04-06 22:41:36 -0300

    responsive iconitos

--- src/features/landing-page/components/OurServices/components/OurServicesFooter.tsx
+++ src/features/landing-page/components/OurServices/components/OurServicesFooter.tsx
@@ -7,7 +7,7 @@ export default function OurServicesFooter (): JSX.Element {
     <div className='flex flex-col lg:flex-row justify-evenly items-center gap-16 lg:gap-0 py-16 px-10'>
 
       <div className='flex flex-col gap-8'>
-        <h4 className='font-ivy text-3xl sm:text-4xl text-[#F2EBDC] text-center lg:text-start font-semibold leading-normal'>
+        <h4 className='font-ivy text-3xl sm:text-4xl xl:text-5xl text-[#F2EBDC] text-center lg:text-start font-semibold leading-normal'>
           Make parenting a<br />
           more{' '}
           <span className='italic'>
@@ -25,20 +25,20 @@ export default function OurServicesFooter (): JSX.Element {
 
       </div>
 
-      <div className='flex flex-col gap-8 items-start max-w-lg text-[#EEEBE3] text-lg sm:text-lg lg:text-xl text-left'>
+      <div className='flex flex-col gap-8 items-start max-w-lg text-[#EEEBE3] text-md sm:text-lg lg:text-xl text-left'>
 
         <div className='flex items-start lg:items-center gap-6'>
-          <Image src='/icons/iconito.svg' width={50} height={50} alt='icon' />
+          <Image src='/icons/iconito.svg' width={50} height={50} alt='icon' className='w-8 h-8 sm:w-10 sm:h-10 lg:w-[50px] lg:h-[50px]' />
           <p><span className='font-bold'>Strengthen Family Bonds -</span> Foster deeper connections between parents and children through understanding and emotional support.</p>
         </div>
 
         <div className='flex items-start lg:items-center gap-6'>
-          <Image src='/icons/iconito.svg' width={50} height={50} alt='icon' className='rotate-45' />
+          <Image src='/icons/iconito.svg' width={50} height={50} alt='icon' className='w-8 h-8 sm:w-10 sm:h-10 lg:w-[50px] lg:h-[50px] rotate-45' />
           <p><span className='font-bold'>Empower Parents with 24/7 Tools & Knowledge -</span> Provide practical strategies and expert-backed insights to navigate behavioral challenges with confidence.</p>
         </div>
 
         <div className='flex items-start lg:items-center gap-6'>
-          <Image src='/icons/iconito.svg' width={50} height={50} alt='icon' />
+          <Image src='/icons/iconito.svg' width={50} height={50} alt='icon' className='w-8 h-8 sm:w-10 sm:h-10 lg:w-[50px] lg:h-[50px]' />
           <p><span className='font-bold'>Create a Balanced, Stress-Free Home -</span> Help families manage daily routines, reduce stress and promote a positive home environment.</p>
         </div>
 
